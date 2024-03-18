d532c33a947c470bfc9b0e862c78ba07197ccbc9
RHellgren <rhellgren@icloud.com>
2024-03-18 20:54:44 +0100

    Setup project

---
+++ Canvas/Canvas/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  Canvas
+//
+//  Created by Robin Hellgren on 18/03/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Canvas/Canvas/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Canvas
+//
+//  Created by Robin Hellgren on 18/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
